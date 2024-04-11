82b67853ff1aa029192e1f876096e3b480d19ec3
bogathi reddy <bogathi.reddy@powerschool.com>
2024-04-11 09:07:44 +0530

    Pointcuts and Wildcard Expressions: Pointcut example.

--- src/AOP/LoggingAspect.java
+++ src/AOP/LoggingAspect.java
@@ -2,13 +2,22 @@ package AOP;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class LoggingAspect {
-    @Before("execution( * AOP.*.get*(..))")
-    private void LoggingAdvisory(){
+    @Before("allgetter()")
+    public void LoggingAdvisory(){
         System.out.println("Advisory : get method is called");
     }
+
+    @Before("allgetter()")
+    public void AnotherLoggingAdvisory(){
+        System.out.println("some advisory");
+    }
+
+    @Pointcut("execution( * AOP.*.get*(..))")
+    public void allgetter(){}
 }
